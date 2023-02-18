f7d37e206cda77eb4fd8dd1382cd7edacee5e008
angelhdzmultimedia <angel.hernandez.12@live.com>
2023-02-17 21:34:49 -0400

    1 file modified, 3 added

--- server/api/count.get.ts
+++ server/api/count.get.ts
@@ -1,8 +1,12 @@
+import { useSession } from 'h3'
+
+const sessionPassword = 'nuxt3sessionnuxt3sessionnuxt3sessionnuxt3session'
+
 export default eventHandler(async (event) => {
   const session = await useSession(event, {
-    password: 'asdasdasdasdfdsfdsfsdfsdfsdfsdfsdfsdfsd',
+    password: sessionPassword,
   })
-  session.update((data) => ({ count: (+data.count ?? 0) + 1 }))
 
+  session.update((data) => ({ count: (+data.count ?? 0) + 1 }))
   return session.data.count
 })
